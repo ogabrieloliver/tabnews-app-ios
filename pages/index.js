78280c3f6bf486f39f7bbaac5aeb65a3bf10ea87
ogabrieloliver <130248077+ogabrieloliver@users.noreply.github.com>
2024-11-19 19:10:50 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Nicolly, te amo ... se você me ama dá um risadinha  </h1>
+  return <h1>Nicolly, te amo ... se você me ama dá um risadinha </h1>;
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;

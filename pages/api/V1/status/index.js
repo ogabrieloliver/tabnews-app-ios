3e42e1e7871265fdb08cb0986e1d675713aee0fe
Gabriel Costa Oliveira <130248077+ogabrieloliver@users.noreply.github.com>
2024-12-18 18:11:15 +0000

    Database Max Connections

--- pages/api/V1/status/index.js
+++ pages/api/V1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ Chave: "Acima da media" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectValue),
+      },
+    },
+  });
 }
 
 export default status;
